a1b6e788ab465f0527fd2d5934bd698572eca607
DinethSheveen <fdineth4000@gmail.com>
2025-12-06 13:51:52 +0000

    VITE : App routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,24 @@
 import "./index.css"
+import {Routes,Route} from "react-router-dom"
+import HomePage from "./Pages/HomePage"
+import Dashboard from "./Pages/Dashboard"
+import Income from "./Pages/Income"
+import Expense from "./Pages/Expense"
+import Sidebar from "./Components/Sidebar"
+import AllTransactions from "./Pages/AllTransactions"
 
 function App() {
   return (
-    <div>App</div>
+    <div className="bg-black min-h-screen text-white flex items-center gap-2 px-2">
+      <Sidebar/>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/all-transactions" element={<AllTransactions/>}/>
+        <Route path="/income" element={<Income/>}/>
+        <Route path="/expense" element={<Expense/>}/>
+      </Routes>
+    </div>
   )
 }
 
