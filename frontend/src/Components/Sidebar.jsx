a0b094d3fbb82f62b5f76024bdd8b6cd1686da44
DinethSheveen <fdineth4000@gmail.com>
2025-12-06 14:24:12 +0000

    VITE : Mapped through the menu links

--- frontend/src/Components/Sidebar.jsx
+++ frontend/src/Components/Sidebar.jsx
@@ -3,10 +3,19 @@ import img from "../../public/CodeSprint08.png"
 import { RxDashboard } from "react-icons/rx";
 import { FaWallet } from "react-icons/fa";
 import { GrTransaction } from "react-icons/gr";
+import { LiaSignOutAltSolid } from "react-icons/lia";
 
 function Sidebar() {
+
+  const menuLinks = [
+    {icon:<RxDashboard/>, text:"Dashboard", endpoint:"/dashboard"},
+    {icon:<FaWallet/>, text:"All-Transactions", endpoint:"/all-transactions"},
+    {icon:<GrTransaction/>, text:"Income", endpoint:"/income"},
+    {icon:<GrTransaction/>, text:"Expense", endpoint:"/expense"}
+  ]
+
   return (
-    <div className='sidebar flex flex-col justify-between min-h-[80vh] border border-white p-4 rounded-[10px]'>
+    <div className='sidebar flex flex-col justify-between min-h-[80vh] border border-white pt-4 overflow-hidden rounded-[10px]'>
       {/* PROFILE SECTION WITH IMAGE */}
       <div className="flex flex-col items-center gap-2">
         <img src={img} alt="" className="rounded-full w-15 h-15"/>
@@ -14,32 +23,22 @@ function Sidebar() {
       </div>
       
       {/* MENU LINKS */}
-      <div className="flex flex-col gap-2">
-        <NavLink to={"/dashboard"} className={({isActive})=>
-          `flex items-center gap-1 transition-all ${isActive?"border-b-3 border-b-gray-500 text-gray-500":""}`}>
-          <RxDashboard/>
-          Dashboard
-        </NavLink>
-        <NavLink to={"/all-transactions"} className={({isActive})=>
-          `flex items-center gap-1 transition-all ${isActive?"border-b-3 border-b-gray-500 text-gray-500":""}`}>
-          <FaWallet/>
-          All-Transactions
-        </NavLink>
-        <NavLink to={"/income"} className={({isActive})=>
-          `flex items-center gap-1 transition-all ${isActive?"border-b-3 border-b-gray-500 text-gray-500":""}`}>
-          <GrTransaction/>
-          Income
-        </NavLink>
-        <NavLink to={"/expense"} className={({isActive})=>
-          `flex items-center gap-1 transition-all ${isActive?"border-b-3 border-b-gray-500 text-gray-500":""}`}>
-          <GrTransaction className="scale-x-[-1]"/>
-          Expense
-        </NavLink>
+      <div className="flex flex-col gap-2 px-4">
+        {menuLinks.map((menuLink)=>{
+          return(
+            <NavLink to={menuLink.endpoint} className={({isActive})=>
+              `flex items-center gap-1 transition-all ${isActive?"border-b-3 border-b-gray-500 text-gray-500 font-bold":""}`}>
+              {menuLink.icon}
+              {menuLink.text}
+            </NavLink>
+          )
+        })}
       </div>
 
       {/* SIGN OUT */}
-      <div>
-        sign out
+      <div className="flex items-center justify-center font-bold gap-2 py-2 w-full bg-gray-800 cursor-pointer hover:text-gray-200 hover:bg-transparent transition-all">
+        <LiaSignOutAltSolid className="text-2xl"/>
+        Sign Out
       </div>
     </div>
   )
