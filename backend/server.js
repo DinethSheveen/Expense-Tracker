77eb16f9752dc2b2bf5e153146c36ca9c1fcd368
DinethSheveen <fdineth4000@gmail.com>
2025-12-06 22:02:12 +0000

    EXPRESS : Init cors

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import "dotenv/config"
 import express from "express"
 import dbConnect from "./Config/dbConfig.js"
 import router from "./Routes/transactionRouter.js"
+import cors from "cors"
 
 // APP DECLARATION
 const app = express() 
@@ -11,6 +12,7 @@ const PORT = process.env.PORT || 3000
 
 // MIDDLEWARE CONFIG
 app.use(express.json())
+app.use(cors())
 
 // ROUTING
 app.use("/api/transactions",router)
