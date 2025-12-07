8d5f017f631e90f43b3ecd588c0bdf836c1b7ea9
DinethSheveen <fdineth4000@gmail.com>
2025-12-07 11:56:16 +0000

    VITE : Renamed the Context Provider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,14 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import {BrowserRouter} from "react-router-dom"
-import GlobalContextProvider from './Hooks/GlobalContextProvider.jsx'
+import TransactionContextProvider from './Hooks/TransactionContextProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <GlobalContextProvider>
+      <TransactionContextProvider>
         <App />
-      </GlobalContextProvider>
+      </TransactionContextProvider>
     </BrowserRouter>
   </StrictMode>,
 )
