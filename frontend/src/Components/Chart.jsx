9dd94db80d43c111f519fa6a84d73860f582a844
DinethSheveen <fdineth4000@gmail.com>
2025-12-08 17:31:23 +0000

    VITE : Installed chartjs and generated a chart to represent the income and expenses

---
+++ frontend/src/Components/Chart.jsx
@@ -0,0 +1,97 @@
+import {Line} from "react-chartjs-2"
+import { Chart as ChartJS, CategoryScale, LinearScale, LineElement,PointElement, Title, Legend, Tooltip } from "chart.js"
+import { useEffect, useState } from "react"
+import {TransactionContext} from "../Hooks/TransactionContextProvider"
+import dayjs from "dayjs"
+import axios from "axios"
+
+ChartJS.register(CategoryScale, LinearScale, LineElement,PointElement, Title, Legend, Tooltip)
+
+function Chart() {
+
+  const dates = []
+  const [incomes, setIncomes] = useState([])
+  const [expenses, setExpenses] = useState([])
+  // const incomeAmounts = []
+  // const expenseAmounts = []
+
+  useEffect(()=>{
+    const fetchIncome = async()=>{
+      try {
+        const response = await axios.get("http://localhost:3000/api/transactions/income/by-date")
+        setIncomes(response.data.message)
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+    const fetchExpense = async()=>{
+      try {
+        const response = await axios.get("http://localhost:3000/api/transactions/expense/by-date")
+        setExpenses(response.data.message)
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+    fetchIncome()
+    fetchExpense()
+  },[])
+
+  
+  // FORMATTING DATES AND PUSHING THEM INTO THE ARRAY
+  
+  incomes.map((income)=>{
+    const formattedDate = dayjs(income.date).format("DD/MM/YYYY")
+    if(!dates.includes(formattedDate)){
+      dates.push(formattedDate)
+    }
+  })
+  
+  expenses.map((expense)=>{
+    const formattedDate = dayjs(expense.date).format("DD/MM/YYYY")
+    if(!dates.includes(formattedDate)){
+      dates.push(formattedDate)
+    }
+  })
+  
+  dates.sort((a,b)=>dayjs(a).valueOf() - dayjs(b).valueOf())
+  
+  const incomeAmounts = dates.map(date => {
+    const matched = incomes.find(income => dayjs(income.date).format("DD/MM/YYYY") === date);
+    return matched ? matched.amount : 0;
+  });
+
+  const expenseAmounts = dates.map(date => {
+    const matched = expenses.find(expense => dayjs(expense.date).format("DD/MM/YYYY") === date);
+    return matched ? matched.amount : 0;
+  });
+
+
+  console.log(expenseAmounts);
+  console.log(incomeAmounts);
+  console.log(dates);
+  
+  
+  const options = {}
+  
+  const data = {
+    labels : dates,
+    datasets : [
+      {
+        label : "Income",
+        data : incomeAmounts,
+        borderColor : "#42f575"
+      },
+      {
+        label : "Expense",
+        data : expenseAmounts,
+        borderColor : "#f00202"
+      }
+    ]
+  }
+
+  return (
+    <div className="h-100"><Line options={options} data={data}/></div>
+  )
+}
+
+export default Chart
--- frontend/src/Pages/Dashboard.jsx
+++ frontend/src/Pages/Dashboard.jsx
@@ -1,8 +1,16 @@
+import {Line} from "react-chartjs-2"
+import {Chart as ChartJS,CategoryScale,LinearScale,PointElement,LineElement,Title,Legend,Tooltip } from "chart.js"
+import Chart from "../Components/Chart"
+ChartJS.register(CategoryScale,LinearScale,PointElement,LineElement,Title,Legend,Tooltip)
 
 function Dashboard() {
+
   return (
     <div className='homepage flex-1 justify-center border border-white min-h-[80vh] rounded-[10px] p-2'>
       <p className='text-2xl font-bold'>Dashboard</p>
+      <div>
+        <Chart/>
+      </div>
     </div>
   )
 }
