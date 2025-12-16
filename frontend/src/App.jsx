fa0ff5f15283ae24ba3c63943b54a7f61ecc4027
DinethSheveen <fdineth4000@gmail.com>
2025-12-16 11:21:23 +0000

    VITE : Deleted protected routes component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,6 @@ import Navbar from "./Components/Navbar"
 import AllTransactions from "./Pages/AllTransactions"
 import Register from "./Pages/Register"
 import Login from "./Pages/Login"
-import ProtectedRoutes from "./Components/ProtectedRoutes"
 
 function App() {
   return (
@@ -17,13 +16,11 @@ function App() {
       <Routes>
         <Route path="/auth/register" element={<Register/>}/>
         <Route path="/auth/login" element={<Login/>}/>
-        {/* <Route element={<ProtectedRoutes/>}> */}
         <Route path="/" element={<HomePage/>}/>
         <Route path="/dashboard" element={<Dashboard/>}/>
         <Route path="/all-transactions" element={<AllTransactions/>}/>
         <Route path="/income" element={<Income/>}/>
         <Route path="/expense" element={<Expense/>}/>
-        {/* </Route> */}
       </Routes>
     </div>
   )
