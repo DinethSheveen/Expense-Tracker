0d31b1893dab9007d8963118e85220ea046270cb
DinethSheveen <fdineth4000@gmail.com>
2025-12-18 23:45:52 +0000

    EXPRESS : static middleware to pass images folder path

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,7 @@ const PORT = process.env.PORT || 3000
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({origin : "http://localhost:5173",credentials:true}))
+app.use("/images",express.static("public/images"))
 
 // ROUTING
 app.use("/api/auth",authRouter)
